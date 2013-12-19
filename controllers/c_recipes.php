f9142b89aefa3f8023afef57b0663429eb6e7c1b
Emily Glass Flannery <emily@emilyflannery.com>
2013-12-18 18:29:21 -0600

    Update 12/18

--- controllers/c_recipes.php
+++ controllers/c_recipes.php
@@ -22,7 +22,9 @@ class recipes_controller extends base_controller {
 	    # Query -- HELP --
 	    $q = "SELECT *
 	    		FROM        users, recipes, user_recipes
-	        	WHERE 		users.id = ".$this->user->id; 
+	        	WHERE 		users.id = user_id
+	        	and 		recipes.id = recipe_id
+	        	and 		users.id = ".$this->user->id;
 
 	    # Run the query, store the results in the variable $posts
 	    $my_recipes = DB::instance(DB_NAME)->select_rows($q);
@@ -42,30 +44,39 @@ class recipes_controller extends base_controller {
 	    $this->template->content = View::instance("v_recipes_index");
 	    $this->template->title   = "All Recipes";
 
-	    # Build the query to get all the recipes
+	    # Query -- HELP --
 	    $q = "SELECT *
-	        FROM recipes";
+	    		FROM 		recipes";
 
-	    # Execute the query to get all the users. 
-	    # Store the result array in the variable $recipes
-	    $recipes = DB::instance(DB_NAME)->select_rows($q);
+	    # Run the query, store the results in the variable $posts
+	    $all_recipes = DB::instance(DB_NAME)->select_rows($q);
 
-	    # Pass data (recipes) to the view
-	    $this->template->content->recipes = $recipes;
+	    # Build the query to figure out what connections does this user already have? 
+	    # I.e. who are they following
+	    $q = "SELECT * 
+	        FROM user_recipes
+	        WHERE user_id = ".$this->user->id;
+
+	    # Execute this query with the select_array method
+	    # select_array will return our results in an array and use the "users_id_followed" field as the index.
+	    # This will come in handy when we get to the view
+	    # Store our results (an array) in the variable $connections
+	    $connections = DB::instance(DB_NAME)->select_array($q, 'recipe_id');
+
+	    # Pass data (users and connections) to the view
+	    $this->template->content->all_recipes = $all_recipes;
+		$this->template->content->connections = $connections;
 
 	    # Render the view
 	    echo $this->template;
 
 	}
 
-
-	/*// CHANGE TO RECIPE_ID_ADDED / RECIPE_ID_REMOVED
-	public function added($recipe_id) {
+	public function add($recipe_id) {
 
 		# Prepare the data array to be inserted
 		$data = Array(
-		    "created" => Time::now(),
-		    "user_id" => $this->user->user_id,
+		    "user_id" => $this->user->id,
 		    "recipe_id" => $recipe_id
 		    );
 
@@ -73,20 +84,18 @@ class recipes_controller extends base_controller {
 		DB::instance(DB_NAME)->insert('user_recipes', $data);
 
 		# Send them back
-		Router::redirect("/recipes/index");
+		Router::redirect("/recipes/my_recipes");
 
 	}
 
-
-	public function removed($recipe_id) {
+	public function remove($recipe_id) {
 
 	    # Delete this connection
-	    $where_condition = 'WHERE user_id = '.$this->user->user_id.' AND recipe_id = '.$recipe_id;
+	    $where_condition = 'WHERE user_id = '.$this->user->id.' AND recipe_id = '.$recipe_id;
 	    DB::instance(DB_NAME)->delete('user_recipes', $where_condition);
 
 	    # Send them back
-	    Router::redirect("/recipes/index");
+	    Router::redirect("/recipes/my_recipes");
 
 	}
-	*/
 }
--- views/v_recipes_index.php
+++ views/v_recipes_index.php
@@ -4,23 +4,23 @@
 	
 	<h2>All Recipes</h2>
 
-	<?php foreach($recipes as $recipe): ?>
+	<?php foreach($all_recipes as $each_recipe): ?>
 	<br />
 
 		<article>
 			<!-- Print this user's name -->
-	    <h3><?=$my_recipe['title']?></h3>
-	    <p><?=$my_recipe['description']?></p>
+	    <h3><?=$each_recipe['title']?></h3>
+	    <p><?=$each_recipe['description']?></p>
 
 	    <h4>
-	     	<?php if(isset($connections[$user['user_id']])): ?>
-	        	<a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
+	     	<?php if(isset($connections[$each_recipe['id']])): ?>
+	        	<a href='/recipes/remove/<?=$recipes['id']?>'>Remove</a>
 
 	    <!-- Otherwise, show the follow link -->
 	    	<?php else: ?>
-	        	<a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
-	    	<?php endif; ?>
-	    </h4>
+	        	<a href='/recipes/add/<?=$each_recipes['id']?>'>Add</a>
+	    	<?php endif;  ?>
+	    </h4> 
 
 		</article>
 
